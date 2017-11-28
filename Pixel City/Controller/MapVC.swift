91a58345b1d3df9a3af7354ae92cb2569fee4ce1
Jeffrey Santana <santana@jefffreys-mini.attlocal.net>
2017-11-27 20:18:27 -0500

    Created simple UI; added extensions to work with mapKit, created functions to center the user

---
+++ Pixel City/Controller/MapVC.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  Pixel City
+//
+//  Created by Jeffrey Santana on 11/26/17.
+//  Copyright © 2017 Jefffrey Santana. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapVC: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
+    //Varibles
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 2000
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+        locationManager.delegate = self
+        
+        configueLocationServices()
+    }
+
+    //Actions
+    @IBAction func centerMapBtnPressed(_ sender: Any) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
+    }
+    
+}
+
+extension MapVC: MKMapViewDelegate {
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+}
+
+extension MapVC: CLLocationManagerDelegate {
+    //check to see if authorized to use location
+    func configueLocationServices() {
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
+}
